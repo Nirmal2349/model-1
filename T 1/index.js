c4da297cc19cc238018bae5246d45c8049a37c66
nirmal <nirmalrajr35@gmail.com>
2021-11-18 10:33:31 +0530

    countdown

--- T 1/index.js
+++ T 1/index.js
@@ -1,13 +1,34 @@
-var board = document.createElement("div");
-board.style.height = "200px";
-board.style.width = "200px";
-
-for (let index = 1; index <= 9; index++) {
-  var box = document.createElement("div");
-  box.style.height = "100px";
-  box.style.width = "100px";
-  box.style.backgroundColor = "green";
-  board.appendChild(box);
-}
-
-document.body.appendChild(board);
+setTimeout(function () {
+  document.getElementById("countdown").innerHTML = "10";
+  setTimeout(function () {
+    document.getElementById("countdown").innerHTML = "9";
+    setTimeout(function () {
+      document.getElementById("countdown").innerHTML = "8";
+      setTimeout(function () {
+        document.getElementById("countdown").innerHTML = "7";
+        setTimeout(function () {
+          document.getElementById("countdown").innerHTML = "6";
+          setTimeout(function () {
+            document.getElementById("countdown").innerHTML = "5";
+            setTimeout(function () {
+              document.getElementById("countdown").innerHTML = "4";
+              setTimeout(function () {
+                document.getElementById("countdown").innerHTML = "3";
+                setTimeout(function () {
+                  document.getElementById("countdown").innerHTML = "2";
+                  setTimeout(function () {
+                    document.getElementById("countdown").innerHTML = "1";
+                    setTimeout(function () {
+                      document.getElementById("countdown").innerHTML =
+                        "Happy Day";
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
