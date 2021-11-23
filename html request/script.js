ebb2b3083c8475c6ff09c324051c9b62c502f4c9
nirmal <nirmalrajr35@gmail.com>
2021-11-23 12:02:11 +0530

    xml http request

---
+++ html request/script.js
@@ -0,0 +1,46 @@
+//xml http request are used to interact with servers via api
+
+//4 steps
+
+//step 01: create a XHR object (XMLHTTPREQUEST) {new=>keyword}
+
+// whole program should have same name
+
+var request = new XMLHttpRequest();
+
+//step 02: open a connection
+
+//first parameter: " GET "
+
+//second parameter: " api URL "
+
+//optional 3rd parameter : " True "
+
+request.open(
+  "GET",
+  "https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json",
+  true
+);
+
+// step 03: sending a connection
+
+request.send();
+
+//step 04: once a data sucess fully loaded from server(200)
+
+request.onload = function () {
+  //data travelling from the server will be in string
+  //need to convert from string to object
+  //request.response:server response
+  var data = JSON.parse(request.response);
+
+  console.log(data);
+
+  // for(var i=0;i<data.length;i++){
+  //     console.log(data[i].name)
+  // }
+
+  for (var key in data) {
+    console.log(data[key].name, data[key].capital, data[key].flag);
+  }
+};
