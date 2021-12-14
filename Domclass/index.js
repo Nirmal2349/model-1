bd642a39003417b5a1adb764b96eccb194dea513
nirmal <nirmalrajr35@gmail.com>
2021-12-14 10:30:36 +0530

    dom

--- Domclass/index.js
+++ Domclass/index.js
@@ -1,15 +1,18 @@
+//defenition with example
+
+
 //how to create html element dynamically
 
 //01
-    let division=document.createElement("div");
+let division = document.createElement("div");
 //<div></div>
 
 //02
-    division.innerHTML = "this is dynamic div";
+division.innerHTML = "this is dynamic div";
 //<div> this is div </div>
 
 //03
-     document.body.append(division);
+document.body.append(division);
 //output in document
 
 //basic
@@ -17,7 +20,7 @@ let division1 = document.createElement("span");
 
 division1.innerHTML = "this is dynamic span";
 
-document.body.append(division1,division);
+document.body.append(division1, division);
 
 //additional element
 let division2 = document.createElement("span");
@@ -25,10 +28,10 @@ let division2 = document.createElement("span");
 //styling:
 // using elementname.style.attributename="value";
 // inline css
-division1.style.backgroundColor="lightgreen";
-division1.style.fontFamily="courier";
+division1.style.backgroundColor = "lightgreen";
+division1.style.fontFamily = "courier";
 
-//div tag added 
+//div tag added
 division2.innerHTML = "<div> inner div added </div>";
 
 document.body.append(division2);
@@ -44,7 +47,7 @@ document.body.append(division3);
 //set attribute ('attributename:value')
 let division4 = document.createElement("span");
 
-division4.setAttribute('class','main')
+division4.setAttribute("class", "main");
 
 division4.innerHTML = "<div> inner div Text </div>";
 
@@ -54,7 +57,7 @@ document.body.append(division4);
 
 let division5 = document.createElement("span");
 
-division5.className="main1";
+division5.className = "main1";
 
 division5.innerHTML = "<div> inner div Text </div>";
 
@@ -65,28 +68,22 @@ document.body.append(division5);
 
 //syntax
 
- //document.getElementById();
-   //document.getElementByName();
-      //document.getElementsByTagName()
+//document.getElementById();
+//document.getElementByName();
+//document.getElementsByTagName()
 
-      //check in browser console 
+//check in browser console
 // let res = document.getElementById("new")
 // console.log(res);
 
-//check in browser console 
-function foo(){
-let res = document.getElementById("new");
-console.log(res);
+//check in browser console
+function foo() {
+  let res1 = document.getElementById("new").value;
+  console.log(res1);
 }
 
-
-//check in browser console 
+//check in browser console
 //output always in array
 
-let res = document.getElementsByClassName("new")
-console.log(res);
-
-
-
-
-
+let res2 = document.getElementsByClassName("new");
+console.log(res2);
